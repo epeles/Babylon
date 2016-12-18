ce38ba3ac428cf4029c5be9b5f8030afe82c9ba2
epeles <epeles@ymail.com>
2016-12-18 15:45:27 -0200

    Add files via upload

---
+++ SINPEP-Corp (Hybrid)/Sinpep-Corp.sql
@@ -0,0 +1,13 @@
+﻿select 'EP' tag,* from sinpep
+union all
+select 'PP' tag,* from sinpep
+union all
+select 'PG' tag,* from sinpep
+union all
+select 'PE' tag,* from sinpep
+union all
+select 'GT' tag,* from sinpep
+union all
+select 'MG' tag,* from sinpep
+union all
+select 'MM' tag,* from sinpep
---
+++ SINPEP-Corp (Hybrid)/updateSINPEP-CORP.cmd
@@ -0,0 +1,2 @@
+del extractor_Log.txt
+"D:\Babylon-Enterprise Server\Builder\Glossary Extractor\bin\GLSExtractor.exe" -build Sinpep-Corp.ini
