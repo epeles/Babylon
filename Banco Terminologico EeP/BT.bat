6eb7a39ef3d3ccbc9b537f80b11393fd56c66dec
epeles <epeles@ymail.com>
2016-12-18 15:37:28 -0200

    Add files via upload

---
+++ Banco Terminologico EeP/BT.bat
@@ -0,0 +1,5 @@
+cls
+d:
+cd "D:\Babylon-Enterprise Server\Projects\Banco Terminologico EeP\"
+del Extractor_Log.txt
+"D:\Babylon-Enterprise Server\Builder\Glossary Extractor\bin\GLSExtractor.exe" -build "Banco Terminologico.ini"
