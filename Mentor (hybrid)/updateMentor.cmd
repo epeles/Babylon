ec9ba7f7bbd6b324ecaa451c5dc7347161b9497a
epeles <epeles@ymail.com>
2016-12-18 15:44:31 -0200

    Add files via upload

---
+++ Mentor (hybrid)/updateMentor.cmd
@@ -0,0 +1,5 @@
+cls
+d:
+cd "D:\Babylon-Enterprise Server\Projects\Mentor (hybrid)\"
+del extractor_Log.txt
+"D:\Babylon-Enterprise Server\Builder\Glossary Extractor\bin\GLSExtractor.exe" -build Mentor.ini
