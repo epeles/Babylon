ea639f767fe53cf3e3d102954b1fa34598b358fd
Eitan Peles <epeles@ymail.com>
2018-06-20 13:54:30 -0300

    Add files via upload

---
+++ SST FI/updateSST_FI.cmd
@@ -0,0 +1,5 @@
+cls
+d:
+cd "D:\Babylon-Enterprise Server\Projects\SST FI\"
+del Extractor_Log.txt
+"D:\Babylon-Enterprise Server\Builder\Glossary Extractor\bin\GLSExtractor.exe" -build SST_FI.ini -installlm
